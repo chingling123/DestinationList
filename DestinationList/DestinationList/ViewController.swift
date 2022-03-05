fe4c32e2752f0fa72a276ff13cb96d2fe2b0d83f
Erik Nascimento <erik.nascimento@servexternos.santander.pt>
2022-03-05 21:18:51 +0000

    add UITableView

--- DestinationList/DestinationList/ViewController.swift
+++ DestinationList/DestinationList/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var viewModel: DestinationViewModelProtocol
+    private lazy var tableview: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        table.separatorStyle = .none
+        return table
+    }()
+    
+    init(viewModel: DestinationViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configure()
+        viewModel.fetchData()
     }
-
 
 }
 
+private extension ViewController {
+    func configure() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "My Trips"
+        addViews()
+    }
+    
+    func addViews() {
+        view.addSubview(tableview)
+        makeConstraints()
+    }
+    
+    func makeConstraints() {
+        tableview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableview.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.destinations?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
