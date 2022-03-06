c9516ba96ef04abed55eabc51152edc79a5ff755
Erik Nascimento <erik.nascimento@servexternos.santander.pt>
2022-03-06 21:32:18 +0000

    some animation

--- DestinationList/DestinationList/View/ViewController.swift
+++ DestinationList/DestinationList/View/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var lastShowCell: Bool = false
+    private var toTop: Bool = false
+    private var currentOffset: CGFloat = 0.0
     private var viewModel: DestinationViewModelProtocol
     private lazy var tableview: UITableView = {
         let table = UITableView()
@@ -31,7 +34,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         viewModel.fetchData()
         configure()
     }
@@ -60,5 +62,38 @@ private extension ViewController {
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if !tableView.isDragging {
+            cell.contentView.layer.opacity = 0
+            cell.contentView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                cell.contentView.layer.opacity = 1
+                cell.contentView.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
+            return
+        }
+        if toTop {
+            cell.contentView.transform = CGAffineTransform(translationX: 0, y: 80)
+        } else {
+            cell.contentView.transform = CGAffineTransform(translationX: 0, y: -80)
+        }
+                
+        UIView.animate(withDuration: 1,
+                       delay: 0.05,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0.1,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { cell.contentView.transform = CGAffineTransform(translationX: 0, y: 0) },
+                       completion: nil)
+    }
     
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        currentOffset = scrollView.contentOffset.y
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let newOffset = scrollView.contentOffset.y
+        toTop = newOffset > currentOffset
+        currentOffset = newOffset
+    }
 }
